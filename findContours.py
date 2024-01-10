6e4a157f21856431dad514da91279a85db92c421
UNNC_Corey.H <corey.huang0103@gmail.com>
2024-01-10 19:32:00 +0800

    Rename

--- findContours.py
+++ findContours.py
@@ -1,27 +1,27 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 
 def findContours(img: np.ndarray, original_img: np.ndarray) -> np.ndarray:
     # 找到装甲板灯条轮廓
-    contours, hierarchy = cv.findContours(
-        img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE
+    contours, hierarchy = cv2.findContours(
+        img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
     )
     imgContours = original_img.copy()  # Create a temporary image for drawing contours
     for cnt in contours:
-        area = cv.contourArea(cnt)
+        area = cv2.contourArea(cnt)
         if area > 200:
-            # cv.drawContours(imgContours, cnt, -1, (255, 0, 0), 3)
-            peri = cv.arcLength(cnt, True)
-            approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
+            # cv2.drawContours(imgContours, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             # print(approx)
-            x, y, w, h = cv.boundingRect(approx)
-            cv.rectangle(imgContours, (x, y), (x + w, y + h), (204, 0, 0), 3)
-            cv.putText(
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(imgContours, (x, y), (x + w, y + h), (204, 0, 0), 3)
+            cv2.putText(
                 imgContours,
                 "Stripe",
                 (x + 5, y - 10),
-                cv.FONT_HERSHEY_COMPLEX_SMALL,
+                cv2.FONT_HERSHEY_COMPLEX_SMALL,
                 1,
                 (204, 0, 0),
                 2,
--- preProcessing.py
+++ preProcessing.py
@@ -1,21 +1,21 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 
 def preProcessing(img: np.ndarray) -> np.ndarray:
     # 图片预处理
-    imgBlur = cv.GaussianBlur(img, (7, 7), 1)  # Gaussian blur
-    imgHSV = cv.cvtColor(imgBlur, cv.COLOR_BGR2HSV)  # Convert to HSV color space
+    imgBlur = cv2.GaussianBlur(img, (7, 7), 1)  # Gaussian blur
+    imgHSV = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2HSV)  # Convert to HSV color space
     h_min, h_max = 73, 118
     s_min, s_max = 86, 226
     v_min, v_max = 128, 255
     lower = np.array([h_min, s_min, v_min])  # Lower HSV threshold
     upper = np.array([h_max, s_max, v_max])  # Upper HSV threshold
-    mask = cv.inRange(imgHSV, lower, upper)  # Create mask
-    # cv.imshow("mask", mask)
-    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert to grayscale
-    imgGray = cv.bitwise_and(imgGray, imgGray, mask=mask)  # Mask the grayscale image
-    imgThresh = cv.adaptiveThreshold(
-        imgGray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2
+    mask = cv2.inRange(imgHSV, lower, upper)  # Create mask
+    # cv2.imshow("mask", mask)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+    imgGray = cv2.bitwise_and(imgGray, imgGray, mask=mask)  # Mask the grayscale image
+    imgThresh = cv2.adaptiveThreshold(
+        imgGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
     )  # Adaptive thresholding
     return imgThresh
