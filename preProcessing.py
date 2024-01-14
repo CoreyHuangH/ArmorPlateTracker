1f632cfda796020b8da54c353fd2ec87a7b9219b
UNNC_Corey.H <corey.huang0103@gmail.com>
2024-01-14 16:03:06 +0800

    Update preProcessing.py

--- preProcessing.py
+++ preProcessing.py
@@ -16,8 +16,12 @@ def preProcessing(img: np.ndarray) -> np.ndarray:
     # cv2.imshow("mask", mask)
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
     imgGray = cv2.bitwise_and(imgGray, imgGray, mask=mask)  # Mask the grayscale image
+    
+    kernel = np.ones((5, 5), dtype=np.uint8)
+    imgDilation = cv2.dilate(imgGray, kernel, iterations=2)  # Dilation
+    imgEroded = cv2.erode(imgDilation, kernel, iterations=1)  # Erosion
     imgThresh = cv2.adaptiveThreshold(
-        imgGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+        imgEroded, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
     )  # Adaptive thresholding
-    # cv2.imshow("imgThresh", imgThresh)
+    cv2.imshow("imgThresh", imgThresh)
     return imgThresh
